9f939532f4e50e75c6dfb0bad956064bde8e7c85
YuChenHeMTL <yuchen7568@gmail.com>
2017-08-01 01:07:51 +0000

    added timer

--- question.rb
+++ question.rb
@@ -2,14 +2,32 @@ module MathGame
   class Question
     def new_question (currentPlayer)
       r = Random.new
-      first_number = r.rand(0...500)
-      second_number = r.rand(0...500)
+      first_number = r.rand(0...50)
+      second_number = r.rand(0...50)
       puts "#{currentPlayer.player}: What does #{first_number} plus #{second_number} equals ?"
-      input = gets.chomp.to_i
-      if input == first_number+second_number
-        puts "#{currentPlayer.player}: Yes ! You are correct !"
+      input = nil
+
+      input_number = Thread.new do
+        input = gets.chomp
+      end
+
+      timer = Thread.new do
+        5.downto(0) do |i|
+          puts "\r00:00:#{'%02d' % i}"
+          sleep 1
+          break if input
+        end
+      end
+
+
+      input_number.join(5)
+      timer.join
+
+      if input.to_i == first_number+second_number
+        puts "#{currentPlayer.player}: Yea Yea... Just Keep Going ... "
       else
-        puts "#{currentPlayer.player}: Please... Seriously ?"
+        insult = ["Please... Seriously ? Are you 8 years old ?", "WTF ? "]
+        puts "#{currentPlayer.player}: Please... Seriously ? Are you 8 years old ?"
         currentPlayer.lives -= 1
       end
     end
